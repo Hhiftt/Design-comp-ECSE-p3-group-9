3af2ba2d93c7cf3551e16958e60331cf8bab568c
Vrishank05 <vpat505@aucklanduni.ac.nz>
2026-07-07 14:09:06 +1200

    Made very basic detection

--- Game/blink.py
+++ Game/blink.py
@@ -1,14 +1,43 @@
 from machine import Pin
 from utime import sleep
 
-pin = Pin("LED", Pin.OUT)
+pin1 = Pin(16, Pin.In)
+pin2 = Pin(17, Pin.In)
+pin3 = Pin(18, Pin.In)
+pin4 = Pin(19, Pin.In)
 
-print("LED starts flashing...")
-while True:
-    try:
-        pin.toggle()
-        sleep(1) # sleep 1sec
-    except KeyboardInterrupt:
-        break
-pin.off()
-print("Finished.")
+boolean1 = Gameactive = False
+
+boolean1 = pin1hit = False
+boolean2 = pin2hit = False
+boolean3 = pin3hit = False
+boolean4 = pin4hit = False
+
+while Gameactive == True:
+    if pin1.value() == 1:
+        print("Target 1 Hit")
+        pin1hit = True
+    else:
+        print("Target 1 Not Hit")
+    pin1hit = False
+
+    if pin2.value() == 1:
+        print("Target 2 Hit")
+        pin2hit = True
+    else:
+        print("Target 2 Not Hit")
+        pin2hit = False
+
+    if pin3.value() == 1:
+        print("Target 3 Hit")
+        pin3hit = True
+    else:
+        print("Target 3 Not Hit")
+        pin3hit = False
+
+    if pin4.value() == 1:
+        print("Target 4 Hit")
+        pin4hit = True
+    else:
+        print("Target 4 Not Hit")
+        pin4hit = False
