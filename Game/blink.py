1bbee07e51ed3fa0748028ede189f343702b4a1b
Vrishank05 <vpat505@aucklanduni.ac.nz>
2026-07-08 11:41:35 +1200

    Made pin pull down so no external noise

--- Game/blink.py
+++ Game/blink.py
@@ -1,10 +1,10 @@
 from machine import Pin
-from utime import sleep
+from utime import sleep_ms
 
-pin1 = Pin(16, Pin.In)
-pin2 = Pin(17, Pin.In)
-pin3 = Pin(18, Pin.In)
-pin4 = Pin(19, Pin.In)
+pin1 = Pin(16, Pin.IN, Pin.PULL_DOWN)
+pin2 = Pin(17, Pin.IN, Pin.PULL_DOWN)
+pin3 = Pin(18, Pin.IN, Pin.PULL_DOWN)
+pin4 = Pin(19, Pin.IN, Pin.PULL_DOWN)
 
 boolean1 = Gameactive = False
 
@@ -13,17 +13,20 @@ boolean2 = pin2hit = False
 boolean3 = pin3hit = False
 boolean4 = pin4hit = False
 
+
 while Gameactive == True:
     if pin1.value() == 1:
         print("Target 1 Hit")
         pin1hit = True
+        sleep_ms(100)
     else:
         print("Target 1 Not Hit")
-    pin1hit = False
+        pin1hit = False
 
     if pin2.value() == 1:
         print("Target 2 Hit")
         pin2hit = True
+        sleep_ms(100)
     else:
         print("Target 2 Not Hit")
         pin2hit = False
@@ -31,6 +34,7 @@ while Gameactive == True:
     if pin3.value() == 1:
         print("Target 3 Hit")
         pin3hit = True
+        sleep_ms(100)
     else:
         print("Target 3 Not Hit")
         pin3hit = False
@@ -38,6 +42,7 @@ while Gameactive == True:
     if pin4.value() == 1:
         print("Target 4 Hit")
         pin4hit = True
+        sleep_ms(100)
     else:
         print("Target 4 Not Hit")
         pin4hit = False
