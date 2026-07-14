e1cf378427d315e95ba12f9b867a24812a4c44df
Vrishank05 <vpat505@aucklanduni.ac.nz>
2026-07-14 12:42:11 +1200

    Set outputs to high then change back to low

--- Game/Main.py
+++ Game/Main.py
@@ -7,6 +7,11 @@ pin2 = Pin(17, Pin.IN, Pin.PULL_DOWN)
 pin3 = Pin(18, Pin.IN, Pin.PULL_DOWN)
 pin4 = Pin(19, Pin.IN, Pin.PULL_DOWN)
 
+pin5 = Pin(25, Pin.OUT, Pin.PULL_DOWN)
+pin6 = Pin(26, Pin.OUT, Pin.PULL_DOWN)
+pin7 = Pin(32, Pin.OUT, Pin.PULL_DOWN)
+pin8 = Pin(33, Pin.OUT, Pin.PULL_DOWN)
+
 
 Gameactive, pin1hit, pin2hit, pin3hit, pin4hit = False, False, False, False, False
 
@@ -22,27 +27,35 @@ while Gameactive == True:
     if pin1.value() == 1:
         print("Target 1 Hit")
         pin1hit = True
+        pin5.value(1)
         sleep_ms(100)
+        pin5.value(0)
     else:
         pin1hit = False
 
     if pin2.value() == 1:
         print("Target 2 Hit")
         pin2hit = True
+        pin6.value(1)
         sleep_ms(100)
+        pin6.value(0)
     else:
         pin2hit = False
 
     if pin3.value() == 1:
         print("Target 3 Hit")
         pin3hit = True
+        pin7.value(1)
         sleep_ms(100)
+        pin7.value(0)
     else:
         pin3hit = False
 
     if pin4.value() == 1:
         print("Target 4 Hit")
         pin4hit = True
+        pin8.value(1)
         sleep_ms(100)
+        pin8.value(0)
     else:
         pin4hit = False
